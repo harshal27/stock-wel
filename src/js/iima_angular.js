c70a48114b2ae553ec0b7f56b4b08a8d48cf3cc0
Amit Chaudhary <chaudhary@Amits-MacBook-Air.local>
2017-10-31 17:57:29 +0530

    fixed for refresh token

---
+++ src/js/iima_angular.js
@@ -0,0 +1,57 @@
+angular.module('StaticWebsite', ['ngSanitize']);
+
+angular.module('StaticWebsite').value('$anchorScroll', null).
+  run(['$window', '$templateCache', function ($window, $templateCache) {
+    var templates = $window.JST,
+        fileName,
+        fileContent;
+
+    for (fileName in templates) {
+        fileContent = templates[fileName];
+        $templateCache.put(fileName, fileContent);
+    }
+}]);
+
+
+angular.module('StaticWebsite').controller('MainController', ['$scope', '$http', '$timeout',
+function($scope, $http, $timeout) {
+
+  $scope.announcements = [
+    {
+      date: "July 25, 2017",
+      title: "Conservation and Upgrade of CR1 Classroom",
+      content: "Mr. Vishwavir Ahuja, MD & CEO, RBL Bank announced significant Infrastructure funding support to IIMA"
+    },
+    {
+      date: "July 25, 2017",
+      title: "Conservation and Upgrade of CR1 Classroom",
+      content: "Mr. Vishwavir Ahuja, MD & CEO, RBL Bank announced significant Infrastructure funding support to IIMA"
+    },
+    {
+      date: "July 25, 2017",
+      title: "Conservation and Upgrade of CR1 Classroom",
+      content: "Mr. Vishwavir Ahuja, MD & CEO, RBL Bank announced significant Infrastructure funding support to IIMA"
+    },
+    {
+      date: "July 25, 2017",
+      title: "Conservation and Upgrade of CR1 Classroom",
+      content: "Mr. Vishwavir Ahuja, MD & CEO, RBL Bank announced significant Infrastructure funding support to IIMA"
+    },
+    {
+      date: "July 25, 2017",
+      title: "Conservation and Upgrade of CR1 Classroom",
+      content: "Mr. Vishwavir Ahuja, MD & CEO, RBL Bank announced significant Infrastructure funding support to IIMA"
+    },
+  ];
+
+  $scope.magazine_urls = [
+    "/images/magazines/1.png",
+    "/images/magazines/2.png",
+    "/images/magazines/3.png",
+    "/images/magazines/4.png",
+    "/images/magazines/5.png",
+    "/images/magazines/6.png",
+    "/images/magazines/7.png",
+    "/images/magazines/8.png",
+  ];
+}]);
